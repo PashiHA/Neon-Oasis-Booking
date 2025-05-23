b1573f4e2281f589c2cb3bc57fd34a88c21f70cb
PashiHA <pavelpashkov69@gmail.com>
2025-05-23 20:00:19 +0300

    fix: added missing emailjs-com dependey

--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
-// server/index.js
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config(); // Подключаем .env
+
 const app = express();
 const PORT = 5000;
+
+const ADMIN_PASSWORD = process.env.REACT_APP_ADMIN_PASSWORD;
 
 app.use(cors());
 app.use(express.json());
 
-let bookings = []; // временно, без базы данных
+let bookings = [];
 
+// Защищённый маршрут
+app.get('/api/bookings', (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader !== `Bearer ${ADMIN_PASSWORD}`) {
+    return res.status(403).json({ error: 'Нет доступа' });
+  }
+
+  res.json(bookings);
+});
+
+// Публичный маршрут для создания брони
 app.post('/api/book', (req, res) => {
   const { name, phone, date, time, activity } = req.body;
   bookings.push({ name, phone, date, time, activity });
   res.status(200).json({ message: 'Бронирование успешно принято!' });
-});
-
-app.get('/api/bookings', (req, res) => {
-  res.json(bookings);
 });
 
 app.listen(PORT, () => {
